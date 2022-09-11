e8c571b99f00bc17ac80ec8af1f0484c8022101e
salaadas <carbonfibersweeper@gmail.com>
2022-09-11 11:36:33 +0700

    remove .emacs.d

--- .emacs.d/elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "ee501b06df03c37555d7e2563b0a2b02f92fd3a9")
